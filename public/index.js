9a3695d054d40884d98e239757f7e93b10650dcf
Mruhankhan <s1942405@online.houstonisd.org>
2022-02-06 12:33:04 -0600

    Work

--- public/index.js
+++ public/index.js
@@ -3,7 +3,7 @@ let buttonTwo = document.getElementById("button-2")
 let buttonThree = document.getElementById("button-3")
 let buttonFour = document.getElementById("button-4")
 let buttonFive = document.getElementById("button-5")
-
+let buttonSix = document.getElementById("button-6")
 buttonOne.addEventListener("click", () => {
   let divOne = document.getElementById("div-1")
   divOne.style.animationName = "aa"
@@ -31,4 +31,10 @@ buttonFive.addEventListener("click", () => {
   let divOne = document.getElementById("div-1")
   divOne.style.animationName = "div-animation-spin-full"
   divOne.style.animationDuration = "0.5s"
+})
+
+buttonSix.addEventListener("click", () => {
+  let divOne = document.getElementById("div-1")
+  divOne.style.animationName = "div-animation-spin-full"
+  divOne.style.animationDuration = "2s"
 })
