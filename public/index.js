71dd3b1fcf3cd814008e300444c9b8021bde9af7
Mruhankhan <s1942405@online.houstonisd.org>
2022-02-06 12:26:09 -0600

    Work

--- public/index.js
+++ public/index.js
@@ -2,6 +2,7 @@ let buttonOne = document.getElementById("button-1")
 let buttonTwo = document.getElementById("button-2")
 let buttonThree = document.getElementById("button-3")
 let buttonFour = document.getElementById("button-4")
+let buttonFive = document.getElementById("button-5")
 
 buttonOne.addEventListener("click", () => {
   let divOne = document.getElementById("div-1")
@@ -24,4 +25,10 @@ buttonFour.addEventListener("click", () => {
   typeBackgroundColor = ["red", "yellow", "blue", "brown", "orange", "green", "pink"]
   let backgroundColor = typeBackgroundColor[Math.floor(Math.random() * typeBackgroundColor.length)]
   document.body.style.backgroundColor = backgroundColor
+})
+
+buttonFive.addEventListener("click", () => {
+  let divOne = document.getElementById("div-1")
+  divOne.style.animationName = "div-animation-spin-full"
+  divOne.style.animationDuration = "0.5s"
 })
