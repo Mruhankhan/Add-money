efac8ea879e6fe877e67b77203faabdfe8183623
Mruhankhan <s1942405@online.houstonisd.org>
2022-02-06 08:45:23 -0600

    Work

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ const app = express()
 app.use(express.static("public"))
 
 app.listen(8000, () => {
-  console.log("App listening on port 3000!")
+  console.log("App listening on port 8000!")
 })
 
-app.get("/", (req, res) => {
-  res.send("s")
-})
+app.get("/", (req, res) => {})
