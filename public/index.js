5075539c26479e4871e40016b4bfb076b143de60
Mruhankhan <s1942405@online.houstonisd.org>
2022-02-06 12:07:35 -0600

    Work

--- public/index.js
+++ public/index.js
@@ -1,6 +1,8 @@
 let buttonOne = document.getElementById("button-1")
 let buttonTwo = document.getElementById("button-2")
 let buttonThree = document.getElementById("button-3")
+let buttonFour = document.getElementById("button-4")
+
 buttonOne.addEventListener("click", () => {
   let divOne = document.getElementById("div-1")
   divOne.style.animationName = "aa"
@@ -16,4 +18,10 @@ buttonThree.addEventListener("click", () => {
   typeColor = ["red", "yellow", "blue", "brown", "orange", "green", "pink"]
   let colors = typeColor[Math.floor(Math.random() * typeColor.length)]
   divOne.style.backgroundColor = colors
+})
+
+buttonFour.addEventListener("click", () => {
+  typeBackgroundColor = ["red", "yellow", "blue", "brown", "orange", "green", "pink"]
+  let backgroundColor = typeBackgroundColor[Math.floor(Math.random() * typeBackgroundColor.length)]
+  document.body.style.backgroundColor = backgroundColor
 })
